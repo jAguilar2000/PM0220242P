2f04fcb71051e400740b839495ffb5d3493a8eea
jAguilar2000 <52142265+jAguilar2000@users.noreply.github.com>
2024-06-03 22:43:00 -0600

    -foto

--- Views/PageInit.xaml.cs
+++ Views/PageInit.xaml.cs
@@ -2,10 +2,48 @@ namespace PM0220242P.Views;
 
 public partial class PageInit : ContentPage
 {
+	FileResult photo;
 	public PageInit()
 	{
 		InitializeComponent();
 	}
+
+	public String GetImage64()
+	{
+        String Base64 = String.Empty;
+
+        if (photo != null)
+		{
+			using (MemoryStream ms = new MemoryStream())
+			{
+				Stream stream = photo.OpenReadAsync().Result;
+				stream.CopyTo(ms);
+				byte[] data = ms.ToArray();
+
+				Base64 = Convert.ToBase64String(data);
+				return Base64;
+			}
+		}
+		return Base64;
+	}
+
+    public byte[] GetImageArray()
+    {
+		byte[] data = new byte[] {};
+        if (photo != null)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                Stream stream = photo.OpenReadAsync().Result;
+                stream.CopyTo(ms);
+                data = ms.ToArray();
+
+                return data;
+            }
+        }
+        return data;
+    }
+
 
     private async void btnaceptar_Clicked(object sender, EventArgs e)
     {
@@ -22,5 +60,15 @@ public partial class PageInit : ContentPage
 			await DisplayAlert("Aviso", "Registron ingresado con éxito !!", "OK");
 		}
 
+    }
+
+    private async void btnfoto_Clicked(object sender, EventArgs e)
+    {
+		photo = await MediaPicker.Default.CapturePhotoAsync();
+
+		if (photo != null) 
+		{
+			string Localizacion = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+		}
     }
 }
