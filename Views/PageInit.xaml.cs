54b7537b15afd3c5d2d667fc0939f506f42f53ec
jAguilar2000 <52142265+jAguilar2000@users.noreply.github.com>
2024-06-05 00:17:34 -0600

    -tomar y guardar foto

--- Views/PageInit.xaml.cs
+++ Views/PageInit.xaml.cs
@@ -2,34 +2,34 @@ namespace PM0220242P.Views;
 
 public partial class PageInit : ContentPage
 {
-	FileResult photo;
-	public PageInit()
-	{
-		InitializeComponent();
-	}
+    FileResult? photo;
+    public PageInit()
+    {
+        InitializeComponent();
+    }
 
-	public String GetImage64()
-	{
+    public String GetImage64()
+    {
         String Base64 = String.Empty;
 
         if (photo != null)
-		{
-			using (MemoryStream ms = new MemoryStream())
-			{
-				Stream stream = photo.OpenReadAsync().Result;
-				stream.CopyTo(ms);
-				byte[] data = ms.ToArray();
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                Stream stream = photo.OpenReadAsync().Result;
+                stream.CopyTo(ms);
+                byte[] data = ms.ToArray();
 
-				Base64 = Convert.ToBase64String(data);
-				return Base64;
-			}
-		}
-		return Base64;
-	}
+                Base64 = Convert.ToBase64String(data);
+                return Base64;
+            }
+        }
+        return Base64;
+    }
 
     public byte[] GetImageArray()
     {
-		byte[] data = new byte[] {};
+        byte[] data = new byte[] { };
         if (photo != null)
         {
             using (MemoryStream ms = new MemoryStream())
@@ -47,28 +47,42 @@ public partial class PageInit : ContentPage
 
     private async void btnaceptar_Clicked(object sender, EventArgs e)
     {
-		var person = new Models.Personas
-		{
-			Nombres = Nombres.Text,
-			Apellidos = Apellidos.Text,
-			FechaNac = FechaNac.Date,
-			Telefono = Telefono.Text
-		};
+        var person = new Models.Personas
+        {
+            Nombres = Nombres.Text,
+            Apellidos = Apellidos.Text,
+            FechaNac = FechaNac.Date,
+            Telefono = Telefono.Text,
+            Foto = GetImage64(),
+        };
 
-		if(await App.DataBase.StorePerson(person) > 0)
-		{
-			await DisplayAlert("Aviso", "Registron ingresado con éxito !!", "OK");
-		}
+        if (await App.DataBase.StorePerson(person) > 0)
+        {
+            await DisplayAlert("Aviso", "Registron ingresado con éxito !!", "OK");
+        }
 
     }
 
     private async void btnfoto_Clicked(object sender, EventArgs e)
     {
-		photo = await MediaPicker.Default.CapturePhotoAsync();
+        try
+        {
+            photo = await MediaPicker.Default.CapturePhotoAsync();
 
-		if (photo != null) 
-		{
-			string Localizacion = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
-		}
+            if (photo != null)
+            {
+                string Localizacion = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+                using Stream sourceStream = await photo.OpenReadAsync();
+                using FileStream imagenlocal = File.OpenWrite(Localizacion);
+
+                FotoImage.Source = ImageSource.FromStream(() => photo.OpenReadAsync().Result);
+
+                await sourceStream.CopyToAsync(imagenlocal);
+            }
+        }
+        catch (Exception ex)
+        {
+            ex.ToString();
+        }
     }
 }
